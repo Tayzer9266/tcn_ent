848ca5b2ddb7c4af0976f50304493d91bea90a32
Tayzer9266 <tnguyen9266@gmail.com>
2025-02-19 22:04:03 -0600

    afdasdf

--- pages/5_Song_Requests.py
+++ pages/5_Song_Requests.py
@@ -1,5 +1,6 @@
 import streamlit as st  
 from sqlalchemy import create_engine
+from sqlalchemy import text
 import pandas as pd
 import base64
 from PIL import Image
@@ -71,7 +72,7 @@ conn = init_connection()
 def execute_procedure(email, song, artist, first_name):
     try:
         # Testing the connection
-        conn.execute("SELECT 1")
+        conn.execute("CALL sp_song_request(%s, %s, %s, %s);", (email, song, artist, first_name))
     except Exception as e:
         st.error(f"Error connecting to the database: {e}")
     # try:
@@ -86,7 +87,7 @@ def execute_procedure(email, song, artist, first_name):
 @st.cache_data(ttl=600)
 def run_query(query):
     # Use SQLAlchemy connection and execute query
-    result = conn.execute(query)
+    result = conn.execute(text(query))
     # Fetch all results and load them into a pandas DataFrame
     rows = result.fetchall()
     if rows is None:
