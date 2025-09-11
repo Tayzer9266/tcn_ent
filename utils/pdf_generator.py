f8b47bc1605a20a39a8d277017e7fb3f3b51dd69
Tay Nguyen <tcnguyen@siepe.com>
2025-09-10 19:13:42 -0500

    asdfasdf

--- utils/pdf_generator.py
+++ utils/pdf_generator.py
@@ -203,64 +203,43 @@ class PDFGenerator:
  
 
 I. PARTIES
-
 This DJ Contract ("Contract" hereinafter) is signed by {dj_name} ("DJ" hereinafter) and {client_name} ("Client" hereinafter) on {contract_date} wherein both parties agreed on the following terms.
 
 II. EVENT DESCRIPTION
-
 This Contract sets out the event details and terms and conditions where DJ {dj_name} will perform.
-
 Will be providing the musical entertainment including any tools need for the sound system to function properly for
-
 Event on {event_date}. Below is the detailed event information:
 
 Performer(s): {dj_name}
-
 Event Title: {event_type}
-
 Date: {event_date}
-
 Start Time: {start_time}
-
 End Time: {end_time}
-
 Location: {event_location}
 
 III. PAYMENT
-
 The total fee which will be paid to DJ under this contract is ${total_fee}. A non-refundable deposit of ${deposit} is required. The balance due is paid by credit card or check at the date of the event unless other arrangements have been agreed upon by {dj_name} to perform from {start_time} until {end_time} on the date of the event. In case there is a need to extend performance, the Client shall pay $50.00 per hour for the extension. Entrance fee, parking and electrical fees will be paid by the Client.
 
 IV. EQUIPMENT
-
 {dj_name} Shall bring the following equipment and personnel:
-
 {equipment_list}
 
 V. TERMINATION OF THE CONTRACT
-
 If the contract is terminated by the Client before the event day, the deposit paid will not be refunded. In case the Contract is terminated on the day of the event, DJ will be entitled to the full contract price and the balance due will be paid on the same day. In both cases, Client shall notify the termination to the DJ in writing.
-
 If the DJ will not be able to perform in an emergency (i.e. accident, health problems, force majeure etc.), the DJ must find a DJ to perform on his behalf and ensure that he fulfills the obligation arising from this contract. If the DJ cannot provide any replacements, DJ shall refund all fees previously paid by the Client, including the deposit.
 
 VI. ENTIRE AGREEMENT
-
 This Contract with any attachments constitutes the complete understanding of the parties to this Contract, regarding the subject matter contained in this Contract, and supersedes all other agreements or arrangements, either oral or in writing.
 
 VII. AMENDMENTS
-
 Any modification or variation of this Contract shall be in writing with the mutual consent of the parties.
-
 Both parties agree to the terms and conditions stated above as demonstrated by their signatures as follows:
 
-
 Signature Signature
-
 {dj_name}
-
 Date: {contract_date}
 
 {client_name}
-
 Date: {contract_date}
 """
 
